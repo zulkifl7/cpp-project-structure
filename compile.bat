1609fc1f352bfe4b5c21238081b8c4445706227d
Zulkifl Ajmal <zulkiflajmal@gmail.com>
2024-05-16 11:05:25 +0530

    File to exicute the files

---
+++ compile.bat
@@ -0,0 +1,19 @@
+@echo off
+
+
+g++ -c source/math_functions.cpp -o bin/math_functions.o
+
+if errorlevel 1 (
+  echo Error: Compilation failed while creating object!
+  exit /b 1
+)
+g++ main.cpp bin/math_functions.o -o tikTakToe.exe
+
+if errorlevel 2 (
+  echo Error: Compilation failed!
+  exit /b 2
+)
+
+tikTakToe.exe
+
+echo Program Exicution Complete! 
