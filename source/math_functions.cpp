097c2b3e15b43685ee8237bb418c18f2d3c8fb3b
Zulkifl Ajmal <zulkiflajmal@gmail.com>
2024-05-16 11:04:56 +0530

    created example file

---
+++ source/math_functions.cpp
@@ -0,0 +1,13 @@
+// math_functions.cpp
+
+#include "../lib/math_functions.h" // Include the header file
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+double multiply(double x, double y)
+{
+    return x * y;
+}
